773e0e8e47f73bf09185b59b5ba2529d461824d9
chaenLIM <limce21@naver.com>
2022-12-23 12:55:25 +0900

    feat: 테마 설정

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+	textColor: '#030303',
+	bgColor: '#fff',
+	contentBgColor: '#f9f9f9',
+	badgeColor: 'rgba(0, 0, 0, 0.05)',
+	endPointColor: '#606060',
+	borderColor: '#d4d4d4'
+};
+
+export const darkTheme: DefaultTheme = {
+	textColor: '#f1f1f1',
+	bgColor: '#0f0f0f',
+	contentBgColor: '#181818',
+	badgeColor: 'rgba(255,255,255,0.1)',
+	endPointColor: '#f1f1f1',
+	borderColor: '#d4d4d4'
+};
